10cde4368ce0a9cb89cf090fe17a8ec4c54c96c8
Faadil <faadil.farick@outlook.com>
2020-06-27 20:08:13 +0530

    Initial commit

---
+++ src/components/SocialFollow/SocialFollow.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div className="social-container">
+      <a
+        href="https://www.facebook.com/ipixel.cloud"
+        className="facebook social"
+        target="_blank"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a
+        href="https://www.twitter.com/ipixel.cloud"
+        className="twitter social"
+        target="_blank"
+      >
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/ipixel.cloud"
+        target="_blank"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+import SocialFollow from "./components/SocialFollow/SocialFollow";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          Welcome to iPixel Business Services
+        </h1>
+        <h2>Website Under Construction...</h2>
+        <Timer />
+        <Preloader />
+        <SocialFollow />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
