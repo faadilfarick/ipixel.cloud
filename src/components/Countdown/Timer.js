26ce05648bda1c2f9028e6d65df5b9e3eda9fee7
Faadil <faadil.farick@outlook.com>
2020-08-31 13:58:02 +0530

    ..

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -42,7 +42,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Aug 19, 2020 21:00:00").getTime();
+    this.deadline = new Date("Sept 19, 2020 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
